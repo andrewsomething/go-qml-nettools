83e6b59570d57d816df0684ca75e469f7c740a93
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-07-20 14:01:59 -0400

    Whitespace clean up.

--- qml-nettools.go
+++ qml-nettools.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/qml.v0"
-	"os"
-	"time"
-	"net"
-	"strconv"
-	"github.com/Cubox-/libping"
+    "fmt"
+    "gopkg.in/qml.v0"
+    "os"
+    "time"
+    "net"
+    "strconv"
+    "github.com/Cubox-/libping"
 )
 
 const (
 )
 
 type App struct {
-	parent     qml.Object
-	Input      qml.Object
-	Message    qml.Object
-	pinging    bool
+    parent     qml.Object
+    Input      qml.Object
+    Message    qml.Object
+    pinging    bool
 }
 
 func (a *App) HandleClick() {
-	input := a.Input.String("text")
-	if len(input) > 0 {
-		go ping(input, a)
-	}
+    input := a.Input.String("text")
+    if len(input) > 0 {
+        go ping(input, a)
+    }
 }
 
 func ping(input string, a *App){
-	a.pinging = true
+    a.pinging = true
     chann := make(chan libping.Response, 100)
     go libping.Pinguntil(input, 0, chann, time.Second)
     for i := range chann {
         if ne, ok := i.Error.(net.Error); ok && ne.Timeout() {
-        	message := fmt.Sprintf("Request timeout for icmp_seq %d\n",
-        	                       strconv.Itoa(i.Seq))
+            message := fmt.Sprintf("Request timeout for icmp_seq %d\n",
+                                   strconv.Itoa(i.Seq))
             //fmt.Printf(message)
             a.Message.Set("text", message)
             continue
@@ -42,9 +42,9 @@ func ping(input string, a *App){
             fmt.Println(i.Error)
             a.Message.Set("text", fmt.Sprintf("Error: %s", i.Error))
         } else {
-        	message := fmt.Sprintf("%d bytes from %s: icmp_seq=%d time=%s\n",
-        	                       i.Readsize, i.Destination, i.Seq, i.Delay)
-        	a.Message.Set("text", message)
+            message := fmt.Sprintf("%d bytes from %s: icmp_seq=%d time=%s\n",
+                                   i.Readsize, i.Destination, i.Seq, i.Delay)
+            a.Message.Set("text", message)
             //fmt.Printf(message)
         }
     }
@@ -53,35 +53,35 @@ func ping(input string, a *App){
 }
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-	os.Exit(0)
+    if err := run(); err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
+    os.Exit(0)
 }
 
 func run() error {
-	qml.Init(nil)
-	engine := qml.NewEngine()
+    qml.Init(nil)
+    engine := qml.NewEngine()
 
-	component, err := engine.LoadFile("main.qml")
-	if err != nil {
-		return err
-	}
+    component, err := engine.LoadFile("main.qml")
+    if err != nil {
+        return err
+    }
 
-	app := App{
-	}
+    app := App{
+    }
 
-	context := engine.Context()
-	context.SetVar("app", &app)
+    context := engine.Context()
+    context.SetVar("app", &app)
 
-	win := component.CreateWindow(nil)
+    win := component.CreateWindow(nil)
 
-	app.Message = win.Root().ObjectByName("message")
-	app.Input = win.Root().ObjectByName("input")
+    app.Message = win.Root().ObjectByName("message")
+    app.Input = win.Root().ObjectByName("input")
 
-	win.Show()
-	win.Wait()
+    win.Show()
+    win.Wait()
 
-	return nil
+    return nil
 }
